0db712c7c3b1591f98aeefa1fab1cbd190de654b
Mshega <viwemrayise99@gmail.com>
2023-02-16 02:11:11 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,10 +4,8 @@
  *main - A program that prints and that piece of art is useful" - Dora Korpar, 2015-10-19
  *Return: 1
  */
-
 int main(void)
 {
-fput(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
-
