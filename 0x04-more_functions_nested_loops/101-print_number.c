0ad84ce48ef5e81dbfb39ade9f74bd7a62e31904
Mshega <viwemrayise99@gmail.com>
2023-02-23 13:07:06 -0800

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,15 +7,20 @@
  */
 void print_number(int n)
 {
-unsigned int k = n;
-if (n < 0)
-{
-n *= -1;
-k = n;
-_putchar('-');
-}
-k /= 10;
-if (k != 0)
-print_number(k);
-_putchar((unsigned int) n % 10 + '0');
+	unsigned int k;
+
+	if (n < 0)
+	{
+		k = -n;
+		_putchar('-');
+	}
+	else
+	{
+		k = n;
+	}
+	if (n / 10)
+	{
+		print_number(k / 10);
+	}
+	_putchar((k % 10) + '0');
 }
