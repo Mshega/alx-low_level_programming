b6105fdc028e37806ec1a67d850af15c60c0793c
Mshega <viwemrayise99@gmail.com>
2023-02-23 14:09:49 -0800

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,7 +10,6 @@
 int main(void)
 
 {
-
         int i;
 
         printf("Infinite loop incoming :(\n");
@@ -21,5 +20,4 @@ int main(void)
         /*}*/
         printf("Infinite loop avoided! \\o/\n");
         return (0);
-
 }
