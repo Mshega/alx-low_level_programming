31d0bd213163c8265af9e3d703da7e2497b28ca6
mshega <viwemrayise99@gmail>
2023-04-08 06:04:12 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -4,13 +4,22 @@
  * print_binary - prints the binary equivalent of a decimal number
  * @n: number to print in binary
  */
-void print_binary(unsigned long int n) 
+void print_binary(unsigned long int n)
 {
 int i;
-unsigned long int mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
-for (i = 0; i < sizeof(unsigned long int) * 8; i++) 
+int counter = 0;
+unsigned long int mask;
+for (i = 63; i >= 0; i--)
 {
-_putchar((n & mask) ? '1' : '0');
-mask >>= 1;
+mask = n >> i;
+if (mask & 1)
+{
+_putchar('1');
+counter++;
 }
+else if (counter)
+_putchar('0');
+}
+if (!counter)
+_putchar('0');
 }
