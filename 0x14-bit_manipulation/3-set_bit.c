6ad0b2dc6d078e6a5d89d057113b8ce7284dbda5
mshega <viwemrayise99@gmail>
2023-04-09 00:28:13 -0700

    3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -5,16 +5,15 @@
  * @n: pointer to the number to change
  * @index: index of the bit to set to 1
  *
- * Return: 1if true, -1 if false
+ * Return: 1 if true, -1 if false
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-const unsigned int MAX_INDEX = 63;
-if (index > MAX_INDEX)
-{
-return (-1);
-}
-unsigned long int mask = 1UL << index;
-*n |= mask;
-return (1);
+	const unsigned int MAX_INDEX = 63;
+
+	if (index > MAX_INDEX)
+		return (-1);
+
+	*n = ((1UL << index) | *n);
+	return (1);
 }
