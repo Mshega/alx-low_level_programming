e24f8bcf03e88c5f8086c8e928a6c95b5dc8445c
mshega <viwemrayise99@gmail.com>
2023-07-06 16:04:52 +0200

    2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,9 +9,12 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-int val;
-if (index > 63)
-return (-1);
-val = (n >> index) & 1;
-return (val);
+	int val;
+
+	if (index > 63)
+		return (-1);
+
+	val = (n >> index) & 1;
+
+	return (val);
 }
